4c02ab3fdbbf2f608f9ce99bf55069b355491ec0
mermaid-sn <elifnur.sahin@nunokullari.com>
2026-01-18 18:07:22 +0300

    Update OuttakeShootCommand.java

--- frc-2026-testrobot/src/main/java/frc/robot/commands/OuttakeCommand/OuttakeShootCommand.java
+++ frc-2026-testrobot/src/main/java/frc/robot/commands/OuttakeCommand/OuttakeShootCommand.java
@@ -1,1 +1,28 @@
+package frc.robot.commands.outtake;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.OuttakeSubsystem;
+
+public class OuttakeShootCommand extends CommandBase {
+    private final OuttakeSubsystem outtake;
+
+    public OuttakeShootCommand(OuttakeSubsystem outtake) {
+        this.outtake = outtake;
+        addRequirements(outtake);
+    }
+
+    @Override
+    public void execute() {
+        outtake.shoot();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        outtake.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
