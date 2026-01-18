f21cc4743c0d838ef4ed99a39ca18e6575fc2daf
dlaince <dilann.capkan@gmail.com>
2026-01-18 12:36:13 +0300

    .

--- frc-2026-testrobot/src/main/java/frc/robot/Robot.java
+++ frc-2026-testrobot/src/main/java/frc/robot/Robot.java
@@ -1,72 +1,51 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+package frc.robot;  //klasör
 
-package frc.robot;
+import edu.wpi.first.wpilibj.TimedRobot;  //Her 20 ms'de belirli fonksiyonlar çağıran ana sınıflardan
+import edu.wpi.first.wpilibj2.command.Command;  /*Interface/ abstract class. Commandler initialize -başladığı an-, execute -çalıştığı sürece-, 
+end -bitti /iptal edildi- ve isFinished -ne zaman bitsin- fonksiyonlarından oluşur (Bilgilendirme hehehe) */
+import edu.wpi.first.wpilibj2.command.CommandScheduler;  //Commandlerin kullanımı, subsystemlerin çakışmaması ve buton kullanımı gbi kısımları denetler
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+public class Robot extends TimedRobot {  //TimedRobot'tan türeme Robot ana sınıfı, Initli fonksiyonlar otomatik çağrılır (Driver Station'da mod değişince otomatik çağrılır)
 
-  private final RobotContainer m_robotContainer;
+    private RobotContainer robotContainer; //robotun beyni denilebilir,boş referans tanımlanmış
+    private Command autonomousCommand;
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
-  }
-
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    @Override
+    public void robotInit() {  //robot ilk açıldığında tek seferliğine çalışır.
+        robotContainer = new RobotContainer();  //Container oluşturuldu
     }
-  }
 
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void autonomousExit() {}
-
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    @Override
+    public void robotPeriodic() {  //Her 20ms'de bir çalışır
+        CommandScheduler.getInstance().run(); //Her 20 ms'de bir çalışır.
     }
-  }
 
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void autonomousInit() {
+        autonomousCommand = robotContainer.getAutonomousCommand(); //RobotContainer'dan command alınır
+        if (autonomousCommand != null) {  //null değilse başlar
+            autonomousCommand.schedule();
+        }
+    }
 
-  @Override
-  public void teleopExit() {}
+    @Override
+    public void teleopInit() {
+        if (autonomousCommand != null) {  //otonomda çalışan command varsa durdurur
+            autonomousCommand.cancel();
+        }
+    }
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    @Override
+    public void disabledInit() {}
+    //encoder reset, led kapatma vs. eklenilenilir
+  
+    @Override
+    public void disabledPeriodic() {}
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void teleopPeriodic() {}
 
-  @Override
-  public void testExit() {}
+    @Override
+    public void autonomousPeriodic() {}
 }
