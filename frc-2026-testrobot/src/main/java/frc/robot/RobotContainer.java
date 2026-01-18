30299b05cb33ee1ca463ecb46d4c43e7932a4454
dlaince <dilann.capkan@gmail.com>
2026-01-18 12:34:11 +0300

    .

--- frc-2026-testrobot/src/main/java/frc/robot/RobotContainer.java
+++ frc-2026-testrobot/src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,113 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.subsystems.*;
+import frc.robot.commands.*;
+import frc.robot.commands.elevator.*;
+import frc.robot.commands.intake.*;
+import frc.robot.commands.outtake.*;
+import frc.robot.commands.climber.*;
+import frc.robot.autonomous.AutoRoutine;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
-  }
 
-  private void configureBindings() {}
+    // Controllers
+    private final XboxController driverController =
+            new XboxController(Constants.OperatorConstants.DRIVER_CONTROLLER_PORT);
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+    private final XboxController operatorController =
+            new XboxController(Constants.OperatorConstants.OPERATOR_CONTROLLER_PORT);
+
+    // Subsystems
+    private final ClimberSubsystem climberSubsystem = new ClimberSubsystem();
+    private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+    private final VisionSubsystem visionSubsystem = new VisionSubsystem();
+    private final ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
+    private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    private final OuttakeSubsystem outtakeSubsystem = new OuttakeSubsystem();
+
+    public RobotContainer() {
+        configureDefaultCommands();
+        configureButtonBindings();
+    }
+
+    // ---------------- DEFAULT COMMANDS ----------------
+    private void configureDefaultCommands() {
+
+        driveSubsystem.setDefaultCommand(
+                new DriveCommand(driveSubsystem, driverController)
+        );
+
+        elevatorSubsystem.setDefaultCommand(
+                new HoldElevatorCommand(elevatorSubsystem)
+        );
+
+        climberSubsystem.setDefaultCommand(
+                new HoldClimberCommand(climberSubsystem)
+        );
+    }
+
+    // ---------------- BUTTON BINDINGS ----------------
+    private void configureButtonBindings() {
+
+        /* ---------- DRIVER ---------- */
+
+        new JoystickButton(driverController, Constants.ButtonConstants.AUTO_ALIGN)
+                .whileTrue(new AutoAlignCommand(driveSubsystem, visionSubsystem));
+
+        // CLIMBER – driver kontrol ediyor
+        new JoystickButton(driverController, Constants.ButtonConstants.CLIMB_UP)
+                .whileTrue(new ClimbUpCommand(climberSubsystem))
+                .onFalse(new ClimbStopCommand(climberSubsystem));
+
+        new JoystickButton(driverController, Constants.ButtonConstants.CLIMB_DOWN)
+                .whileTrue(new ClimbDownCommand(climberSubsystem))
+                .onFalse(new ClimbStopCommand(climberSubsystem));
+
+        /* ---------- OPERATOR ---------- */
+
+        // Climber zero
+        new JoystickButton(operatorController, Constants.ButtonConstants.CLIMBER_ZERO)
+                .onTrue(new ZeroClimberCommand(climberSubsystem));
+
+        // Intake
+        new JoystickButton(operatorController, Constants.ButtonConstants.INTAKE_IN)
+                .whileTrue(new IntakeInCommand(intakeSubsystem))
+                .onFalse(new IntakeStopCommand(intakeSubsystem));
+
+        new JoystickButton(operatorController, Constants.ButtonConstants.INTAKE_OUT)
+                .whileTrue(new IntakeOutCommand(intakeSubsystem))
+                .onFalse(new IntakeStopCommand(intakeSubsystem));
+
+        // Elevator levels
+        new JoystickButton(operatorController, Constants.ButtonConstants.ELEVATOR_L1)
+                .onTrue(new SetElevatorLevelCommand(elevatorSubsystem, 1));
+
+        new JoystickButton(operatorController, Constants.ButtonConstants.ELEVATOR_L2)
+                .onTrue(new SetElevatorLevelCommand(elevatorSubsystem, 2));
+
+        new JoystickButton(operatorController, Constants.ButtonConstants.ELEVATOR_L3)
+                .onTrue(new SetElevatorLevelCommand(elevatorSubsystem, 3));
+
+        new JoystickButton(operatorController, Constants.ButtonConstants.ELEVATOR_ZERO)
+                .onTrue(new ZeroElevatorCommand(elevatorSubsystem));
+
+        // Outtake
+        new JoystickButton(operatorController, Constants.ButtonConstants.SHOOT)
+                .whileTrue(new OuttakeShootCommand(outtakeSubsystem))
+                .onFalse(new OuttakeStopCommand(outtakeSubsystem));
+    }
+
+    // ---------------- AUTONOMOUS ----------------
+    public Command getAutonomousCommand() {
+        return new AutoRoutine(
+                driveSubsystem,
+                visionSubsystem,
+                elevatorSubsystem,
+                outtakeSubsystem
+        );
+    }
 }
